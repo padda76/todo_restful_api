9500b4cd674fe1d261fe3eb64982bfd606f45d87
Odd A Hartvigsen <padda76@users.noreply.github.com>
2018-05-10 13:45:11 +0200

    Added todo model, context and controller

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+        public long UserId { get; set; }
+    }
+}
